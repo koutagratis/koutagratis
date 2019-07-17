60c0c7dd4e429678d7073036ec49539373d467fa
koutagratis <52998341+koutagratis@users.noreply.github.com>
2019-07-17 16:16:07 +0700

    koutagratis.sh

---
+++ koutagratis.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+#tools kuota gratis
+#by acep
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $red    "|       TOOLS KUOTA GRATIS          |"
+echo  $red    "|                                   |"
+echo  $red    "| Author        : Dyxa#ABCD         |"
+echo  $red    "| Contact       : 089676666667      |"
+echo  $red    "| My Team       : Rabbit Cyber team |.                |"
+echo  $red    "| Thanks To     : Al Member         |"
+echo  $blue "<|======================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Telkomsel.                 $blue ||"
+echo $blue "|| $green 2. Axis     			       $blue ||"
+echo $blue "|| $green 3. Indosat	   			  $blue ||"
+echo $blue "|| $green 4. Tri			             $blue ||"
+echo $blue "|| $green 0. Exit The Program           $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@dyxa#ABCD=•>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──[Berapa GB?]>"
+read -p"╰───────root@dyxa#ABCD=•>" kntl;
+sleep 1
+echo $green"╭──[masukan nomor]>"
+read -p"╰───────root@dyxa#ABCD=•>" memeq;
+sleep 1
+echo $red "Loading"
+sleep 2
+echo  $blue "Tunggu Beberapa Menit , Nanti Kouta Akan Bertambah"
+fi
